cae9c3cfc7d1d005b4049aea9608f80c8ce53847
yeojin Kim <jjiny2022@kakao.com>
2022-08-04 01:50:39 +0900

    220803

--- shooting game/js/main.js
+++ shooting game/js/main.js
@@ -31,6 +31,39 @@ function loadImage(){
 
 }
 
+let keysDown={};
+function setupKeyboardListener(){
+  document.addEventListener('keydown',function(event){
+    // console.log("무슨 키가 눌렸어?", event.keyCode)
+    keysDown[event.keyCode] = true;
+    // console.log("키다운객체에 들어간 값은?" ,keysDown);
+  });
+  document.addEventListener('keyup',function(event){
+    delete keysDown[event.keyCode]
+    console.log("버튼 클릭후",keysDown);
+  })
+}
+
+
+function update(){
+  if(39 in keysDown){
+    spaceshipX +=5; //우주선의 속도
+  }//right
+
+  if(37 in keysDown){
+    spaceshipX -=5;
+  } //left
+  if(spaceshipX <= 0){
+    spaceshipX = 0
+  }
+  if(spaceshipX >= canvas.width-64){
+    spaceshipX = canvas.width-64;
+  }
+  //우주선의 좌표값이 무한대로 업데이트가 되는게 아닌! 경기장 안에서만 있게하려면
+
+
+
+}
 
 function render(){
   ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
@@ -38,11 +71,17 @@ function render(){
 }
 
 function main(){
-  render()
-  console.log("animation calls main function")
+  update(); //좌표값을 업데이트하고
+  render(); //그려주고
+  // console.log("animation calls main function")
   requestAnimationFrame(main)
 }
 
 
 loadImage();
+setupKeyboardListener();
+
 main();
+
+//우주선이 오른쪽으로 간다 : x좌표의 값이 증가한다.
+//우주선이 왼쪽 간다 : x좌표의 값이 감소한다.
